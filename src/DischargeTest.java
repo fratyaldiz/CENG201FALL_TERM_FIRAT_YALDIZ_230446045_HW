975184f9c49a4c7e52743d60f62cc57da6d301fc
fratyaldiz <firatylz2004@hotmail.com>
2025-12-23 13:47:43 +0300

    Initial commit

--- src/DischargeTest.java
+++ src/DischargeTest.java
@@ -1,12 +1,13 @@
-public class DischrageTest {
+public class DischargeTest {
     public static void main(String[] args) {
         DischargeStack stack = new DischargeStack();
 
         //Add 5 discharge records.
-        for(int i =1;i<=5;i++) {
-            stack.push(new DischargeRecord(i));
-
-        }
+        stack.push(new DischargeRecord(1));
+        stack.push(new DischargeRecord(2));
+        stack.push(new DischargeRecord(3));
+        stack.push(new DischargeRecord(4));
+        stack.push(new DischargeRecord(5));
 
         //Process (pop) 2 of them.
         stack.pop();
